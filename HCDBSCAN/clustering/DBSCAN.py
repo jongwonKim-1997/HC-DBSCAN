f42c8fa663f91d7d23306b36e166dd663af1130f
jongwonKim-1997 <77820101+jongwonKim-1997@users.noreply.github.com>
2022-06-16 13:20:26 +0900

    change3

--- HCDBSCAN/clustering/DBSCAN.py
+++ HCDBSCAN/clustering/DBSCAN.py
@@ -1,5 +1,7 @@
 import sklearn
 
+import sklearn.cluster
+
 def clustering(clustering_method = 'dbscan',hyp_dict=None,lambda1 =1):
     if clustering_method == 'kmeans':
         return sklearn.cluster.KMeans(**hyp_dict)
